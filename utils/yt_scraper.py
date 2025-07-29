aeaad908583dc54c6bd927b3048cd9aa09e9031f
강희지 <shine_g_0517@naver.com>
2025-07-29 15:12:04 +0900

    chore : 주석 제거 및 코드 정리

--- utils/yt_scraper.py
+++ utils/yt_scraper.py
@@ -8,7 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 class YouTubeScraper:
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument("--headless")  # UI 없이 실행
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
